9ce74016e85c996079f09a4003ab06220531db13
Artur232m <72353548+artur232m@users.noreply.github.com>
2021-03-25 18:06:45 +0300

    HomeWork1&HomeWork2

---
+++ src/HomeWork1/HomeWork1.java
@@ -0,0 +1,8 @@
+package HomeWork1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        String name = " Artur";
+        System.out.println("Hello World\n***********\nMy name is\n\t" + name + "!");
+    }
+}
