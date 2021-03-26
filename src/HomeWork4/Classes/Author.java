d41fe899a5a44c65244845f530f026a9bc382964
Artur232m <72353548+artur232m@users.noreply.github.com>
2021-03-26 18:37:35 +0300

    HomeWork3.26.03

---
+++ src/HomeWork4/Classes/Author.java
@@ -0,0 +1,37 @@
+package HomeWork4.Classes;
+
+public class Author extends Book{
+    private String name;
+    private String lastName;
+    private int age;
+    private String country;
+    public void setAuthor(String name, String lastName, int age,String country){
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String toString(){
+        return "Author \nName : " + getName() + "\nLast Name : " + getLastName() + "\nAge : " + getAge() + "\nCountry : " + getCountry()
+                + "\n" + bookToString() + "\n" +genreToString() + "\n**************************" ;
+
+
+    }
+}
---
+++ src/HomeWork4/Classes/Book.java
@@ -0,0 +1,16 @@
+package HomeWork4.Classes;
+
+public class Book extends Genre{
+   private String nameBook ;
+
+    public String getNameBook() {
+        return nameBook;
+    }
+
+    public void setNameBook(String nameBook){
+        this.nameBook = nameBook;
+    }
+    public String bookToString(){
+        return "Name Book :" +getNameBook();
+    }
+}
