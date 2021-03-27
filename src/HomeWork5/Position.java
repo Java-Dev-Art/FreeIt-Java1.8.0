22e9b82cde679e5e8fbdd3c5d4dd9575c8be4bc2
Artur232m <72353548+artur232m@users.noreply.github.com>
2021-03-27 03:22:06 +0300

    HomeWork5/27

---
+++ src/HomeWork5/Position.java
@@ -0,0 +1,98 @@
+package HomeWork5;
+
+import java.util.Scanner;
+
+public class Position {
+    static int a = 3;
+    static int b = 3;
+    static char[][] game = new char[a][b];
+    char choiceGame;
+
+    public static void set1P(int i){
+
+        if (i == 1){
+            game[0][0] = 'x';
+        }else if (i == 0){
+            game[0][0] = '0';
+        }
+
+    }
+    public static void set2P(int i){
+        if (i == 1) {
+            game[0][1] = 'x';
+        }else if (i == 0){
+            game[0][1] = '0';
+        }
+
+    }
+    public static void set3P(int i){
+        if (i == 1){
+            game[0][2] = 'x';
+        }else if (i == 0){
+            game[0][2] = '0';
+        }
+
+    }
+    public static void set4P(int i){
+
+        if (i == 1){
+            game[1][0] = 'x';
+        }else if (i == 0){
+            game[1][0] = 'x';
+        }
+
+    }
+    public static void set5P(int i){
+
+        if (i == 1){
+            game[1][1] = 'x';
+        }else if (i == 0){
+            game[1][1] = '0';
+        }
+
+    }
+    public static void set6P(int i){
+
+        if (i == 1){
+            game[1][2] = 'x';
+        }else if (i == 0){
+            game[1][2] = '0';
+        }
+
+    }
+    public static void set7P(int i){
+
+        if (i == 1){
+            game[2][0] = 'x';
+        }else if (i == 0){
+            game[2][0] = '0';
+        }
+
+    }
+    public static void set8P(int i){
+
+        if (i == 1){
+            game[2][1] = 'x';
+        }else if (i == 0){
+            game[2][1] = '0';
+        }
+
+    }
+    public static void set9P(int i){
+
+        if (i == 1){
+            game[2][2] = 'x';
+        }else if (i == 0){
+            game[2][2] = '0';
+        }
+
+    }
+    public static void massToString(){
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++) {
+                System.out.print(game[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
--- src/HomeWork5/Tictactoe.java
+++ src/HomeWork5/Tictactoe.java
@@ -2,8 +2,9 @@ package HomeWork5;
 
 import java.util.Scanner;
 
-public class Tictactoe {
+public class Tictactoe extends Position {
     public static void main(String[] args) throws java.util.InputMismatchException {
+
 
         int a = 3;
         int b = 3;
@@ -21,124 +22,122 @@ public class Tictactoe {
             }
             System.out.println();
         }
-        char choice;
-        char[][] game = new char[a][b];
-        char choiceGame;
+        int choice;
+//        char[][] game = new char[a][b];
+        int choiceGame;
         Scanner scanner = new Scanner(System.in);
-        do {
+//        do {
             System.out.println("Поиграем в игру :");
             System.out.println("Сверху поле дла игры");
             System.out.println("Что бы поставить X или 0\nнужно выбрать нужную цифру");
             System.out.println("Да победит сильнейший!!!!");
             System.out.println("Ввидите цыфру : ");
-            choice = (char) scanner.nextInt();
-        } while (choice < 1 || choice > 9);
-        Scanner scanner1 = new Scanner(System.in);
+            choice = scanner.nextInt();
+//        } while (choice < 1 || choice > 9);
 
-        System.out.println("Ввидите 1 ( = х ) или 0 :");
 
-        while (true){
-            choiceGame = (char) scanner1.nextInt();
-            switch (choice) {
-                case '1':
-                    switch (choiceGame) {
-                        case '1':
-                            game[0][0] = 'x';
-                            break;
-                        case '0':
-                            game[0][0] = '0';
-                            break;
-                    }
-                    System.out.println("Ввидите цыфру ");
-                    continue;
-                case '2':
-                    switch (choiceGame) {
-                        case '1':
-                            game[0][1] = 'x';
-                            continue;
-                        case '0':
-                            game[0][1] = '0';
-                            continue;
-                    }
-                    continue;
-                case '3':
-                    switch (choiceGame) {
-                        case '1':
-                            game[0][2] = 'x';
-                            continue;
-                        case '0':
-                            game[0][2] = '0';
-                            continue;
-                    }
-                    continue;
-                case '4':
-                    switch (choiceGame) {
-                        case '1':
-                            game[1][0] = 'x';
-                            continue;
-                        case '0':
-                            game[1][0] = '0';
-                            continue;
-                    }
-                    continue;
-                case '5':
-                    switch (choiceGame) {
-                        case '1':
-                            game[1][1] = 'x';
-                            continue;
-                        case '0':
-                            game[1][1] = '0';
-                            continue;
-                    }
-                    continue;
-                case '6':
-                    switch (choiceGame) {
-                        case '1':
-                            game[1][2] = 'x';
-                            continue;
-                        case '0':
-                            game[1][2] = '0';
-                            continue;
-                    }
-                    continue;
-                case '7':
-                    switch (choiceGame) {
-                        case '1':
-                            game[2][0] = 'x';
-                            continue;
-                        case '0':
-                            game[2][0] = '0';
-                            continue;
-                    }
-                    continue;
-                case '8':
-                    switch (choiceGame) {
-                        case '1':
-                            game[2][1] = 'x';
-                            continue;
-                        case '0':
-                            game[2][1] = '0';
-                            continue;
-                    }
-                    continue;
-                case '9':
-                    switch (choiceGame) {
-                        case '1':
-                            game[2][2] = 'x';
-                            continue;
-                        case '0':
-                            game[2][2] = '0';
-                            continue;
-                    }
-                    continue;
-                default:
-                    System.out.println("Игра закончена....");
-                    break;
-            }
-            System.out.println();
+//        System.out.println("Ввидите 1 ( = х ) или 0 :");
 
+    while (true){
+
+        switch (choice){
+            case 1:
+                System.out.println("Ввидите 1 ( = х ) или 0 :");
+                set1Position();
+                break;
+            case 2:
+                set2Position();
+                break;
+            case 3:
+                set3Position();
+                break;
+            case 4:
+                set4Position();
+                break;
+            case 5:
+                set5Position();
+                break;
+            case 6:
+                set6Position();
+                break;
+            case 7:
+                set7Position();
+                break;
+            case 8:
+                set8Position();
+                break;
+            case 9:
+                set9Position();
+                break;
+            default:
+                massToString();
+                break;
         }
+    }
 
+    }
+    public static void set1Position(){
 
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+//        System.out.println("Ввидите 1 ( = х ) или 0 :");
+        set1P(position);
+        System.out.println("Ввидите следующий номер : ");
+    }
+    public static void set2Position(){
+//        System.out.println("Ввидите 1 ( = х ) или 0 :");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set2P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set3Position(){
+        System.out.println("Ввидите 1 ( = х ) или 0 :");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set3P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set4Position(){
+        System.out.println("Ввидите 1 ( = х ) или 0 :");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set4P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set5Position(){
+        System.out.println("Ввидите 1 ( = х ) или 0 :");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set5P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set6Position(){
+        System.out.println("Ввидите 1 ( = х ) или 0 :");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set6P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set7Position(){
+        System.out.println("1 or 0 ");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set7P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set8Position(){
+        System.out.println("1 or 0 ");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set8P(position);
+        System.out.println("Ввидите следующий номер :");
+    }
+    public static void set9Position(){
+        System.out.println("1 or 0 ");
+        Scanner scanner = new Scanner(System.in);
+        int position = scanner.nextInt();
+        set9P(position);
+        System.out.println("Ввидите следующий номер :");
     }
 }
