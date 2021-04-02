acdc7d999f980f07bbfb3e43614949e40ef8facf
Artur232m <72353548+artur232m@users.noreply.github.com>
2021-04-02 13:48:46 +0300

    newTic

--- src/HomeWork5/Tictactoe.java
+++ src/HomeWork5/Tictactoe.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Tictactoe extends Position {
+public class Tictactoe  {
     public static boolean boolGame = false;
     public static void main(String[] args) throws java.util.InputMismatchException, IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
