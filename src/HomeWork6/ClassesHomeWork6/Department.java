6ed053f0a96582b426bb12900997e18092606dca
Artur232m <72353548+artur232m@users.noreply.github.com>
2021-04-13 15:19:13 +0300

    HomeWorkC

---
+++ src/HomeWork6/ClassesHomeWork6/Department.java
@@ -0,0 +1,38 @@
+package HomeWork6.ClassesHomeWork6;
+
+import java.util.*;
+
+public class Department {
+    private String nameDep;
+    private int id;
+    private MyDepartment depart;
+    private ArrayList<MyDepartment> departments;
+
+    public Department(String nameDep, int id) {
+        this.nameDep = nameDep;
+        this.id = id;
+    }
+    public static class MyDepartment{
+        private String nameWorker;
+        private String workerPosition;
+
+        public String getNameWorker() {
+            return nameWorker;
+        }
+
+        public void setNameWorker(String nameWorker) {
+            this.nameWorker = nameWorker;
+        }
+
+        public String getWorkerPosition() {
+            return workerPosition;
+        }
+
+        public void setWorkerPosition(String workerPosition) {
+            this.workerPosition = workerPosition;
+        }
+        public String toStringDepart(){
+            return "Имя :" + getNameWorker() + "\nДолжность :" + getWorkerPosition();
+        }
+    }
+}
